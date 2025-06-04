6fbb3d761ab333019051fbae0d9671d4aa405283
Natali Schers <schersnatali@gmail.com>
2025-06-04 01:00:51 -0300

    [04/06] TDD - Test Driven Development

--- jornadamilhas/src/JornadaMilhasV1/Modelos/OfertaViagem.cs
+++ jornadamilhas/src/JornadaMilhasV1/Modelos/OfertaViagem.cs
@@ -7,12 +7,29 @@ using JornadaMilhasV1.Validador;
 
 namespace JornadaMilhasV1.Modelos;
 
-public class OfertaViagem: Valida
+public class OfertaViagem : Valida
 {
+    private double desconto;
+    public const double DESCONTO_MAXIMO = 0.7;
+
     public int Id { get; set; }
-    public Rota Rota { get; set; } 
+    public Rota Rota { get; set; }
     public Periodo Periodo { get; set; }
     public double Preco { get; set; }
+    public double Desconto {
+        get => desconto;
+        set {
+            desconto = value;
+
+            if (desconto >= Preco)
+            {
+                Preco *= (1 - DESCONTO_MAXIMO);
+            } else if (desconto > 0)
+            {
+                Preco -= desconto;
+            }
+        } 
+    }
 
 
     public OfertaViagem(Rota rota, Periodo periodo, double preco)
@@ -33,10 +50,11 @@ public class OfertaViagem: Valida
         if (!Periodo.EhValido)
         {
             Erros.RegistrarErro(Periodo.Erros.Sumario);
-        } else if (Rota == null || Periodo == null)
+        }
+        else if (Rota == null || Periodo == null)
         {
             Erros.RegistrarErro("A oferta de viagem não possui rota ou período válidos.");
-        } 
+        }
         else if (Preco <= 0)
         {
             Erros.RegistrarErro("O preço da oferta de viagem deve ser maior que zero.");
---
+++ jornadamilhas/tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,63 @@
+﻿using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 5), new DateTime(2024, 2, 1));
+            double precoOriginal = 100.0;
+            double desconto = 20.00;
+            double precoComDesconto = precoOriginal - desconto;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // Act
+            oferta.Desconto = desconto;
+
+            // Assert
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+
+        [Fact]
+        public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 5), new DateTime(2024, 2, 1));
+            double precoOriginal = 100.0;
+            double desconto = 120.00;
+            double precoComDesconto = 30;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // Act
+            oferta.Desconto = desconto;
+
+            // Assert
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+
+        [Fact]
+        public void RetornaPrecoOriginalQuandoValorDescontoNegativo()
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemA", "DestinoB");
+            Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double precoOriginal = 100.00;
+            double desconto = -20.00;
+            double precoComDesconto = precoOriginal;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // Act
+            oferta.Desconto = desconto;
+
+            // Assert
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+    }
+}
